e0451a95b9b42ed4545e5b97832692477a79adbf
youssef <youssef.mostafa.3112@gmail.com>
2021-09-14 19:14:07 +0200

    finished questions screens

---
+++ lib/screens/questions/questions-list-tile.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class QuestionsListTile extends StatefulWidget {
+  final String title;
+  final String body;
+  final String date;
+  final int num_of_answers;
+  //final String subtitle2;
+  //final String subtitle3;
+  Function ontap;
+
+  QuestionsListTile(
+      this.title, this.body, this.ontap, this.num_of_answers, this.date);
+  @override
+  _QuestionsListTileState createState() => _QuestionsListTileState();
+}
+
+class _QuestionsListTileState extends State<QuestionsListTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsetsDirectional.only(bottom: 10),
+      decoration: BoxDecoration(
+        color: Color(0xff181818),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: ListTile(
+        isThreeLine: true,
+        onTap: widget.ontap,
+        minVerticalPadding: 10,
+        /*leading: CircleAvatar(
+          radius: 20,
+          child: Icon(Icons.account_circle),
+        ),*/
+        title: Padding(
+          padding: const EdgeInsets.only(right: 8.0, top: 8.0, bottom: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 20,
+                    child: Icon(Icons.account_circle),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.title,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'assets/fonts/Changa-Bold.ttf',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        widget.date,
+                        style: TextStyle(
+                          color: Colors.amber,
+                          fontSize: 12,
+                          fontFamily: 'assets/fonts/Changa-Bold.ttf',
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              /*Text(
+                widget.date,
+                style: TextStyle(
+                  color: Colors.amber,
+                  fontSize: 12,
+                  fontFamily: 'assets/fonts/Changa-Bold.ttf',
+                ),
+              ),*/
+            ],
+          ),
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              widget.body,
+              style: TextStyle(
+                color: Colors.grey,
+                fontFamily: 'assets/fonts/Changa-Bold.ttf',
+                fontSize: 15,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: widget.ontap,
+                  child: Text(
+                    widget.num_of_answers.toString() + " Answers",
+                    style: TextStyle(
+                      color: Colors.amber,
+                      fontFamily: 'assets/fonts/Changa-Bold.ttf',
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
