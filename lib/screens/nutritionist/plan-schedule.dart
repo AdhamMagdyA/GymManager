ec5633b63da5898d50827950dc8c99175be81708
ossyahya60 <ossyahya60@gmail.com>
2021-09-22 13:48:59 +0200

    A small fix in member

--- lib/screens/nutritionist/plan-schedule.dart
+++ lib/screens/nutritionist/plan-schedule.dart
@@ -237,34 +237,6 @@ class PlanSchedule extends StatelessWidget {
                 ),
             ],
           ),
-          Padding(
-            padding: EdgeInsets.only(
-              top: 10,
-              left: 10,
-            ),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Container(
-                  height: 42,
-                  width: 42,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                  ),
-                  child: Center(
-                    child: Icon(
-                      Icons.arrow_back,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
         ],
       ),
     );
